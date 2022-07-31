729675f84e6040141f195a21ff72b47f218454a0
ygaum8 <92094715+ygaum8@users.noreply.github.com>
2022-07-31 15:17:10 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+window.alert('Bem vindo ao meu primeiro site profissonal =)')
+
+
+
+
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+const cartoes = document.querySelectorAll('.cartao');
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado(){
+  
+  const cartaoSelecionado = document.querySelector('.selecionado');
+  cartaoSelecionado.classList.remove('selecionado');
+
+}
+
+function mostarCartao(indiceCartao){
+
+  cartoes[indiceCartao].classList.add('selecionado');
+}
+
+btnAvancar.addEventListener('click', function () {
+
+  if(cartaoAtual === cartoes.length - 1) return;
+
+ esconderCartaoSelecionado();
+
+  cartaoAtual++;
+ mostarCartao(cartaoAtual);
+
+});
+
+btnVoltar.addEventListener('click', function () {
+
+  if(cartaoAtual === 0) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostarCartao(cartaoAtual);
+
+});
